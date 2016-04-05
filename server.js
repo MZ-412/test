2aec0e737892520fe2140196866d58246d7bff83
root <root@system.virt.bsd-console.com>
2016-04-05 22:39:01 +0300

    2comm

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
-var http = require('http');
-var static = require('node-static');
-var file = new static.Server('.');
-
-http.createServer(function(req, res) {
-  file.serve(req, res);
-}).listen(8080);
-
-console.log('Server running on port 8080');
+console.log('Hello World');
